118b3a1f9eae6cfbc2a549daccf485771c3c3c32
majroy <majidroyan93@gmail.com>
2018-03-23 22:58:17 +0000

    done all week for exercises and week4-practises

--- week-4-practise/level-1/exercise-1.js
+++ week-4-practise/level-1/exercise-1.js
@@ -6,7 +6,7 @@ and for each object, `console.log()` out the sentence:
 
 Here is the array:
 
-*/ 
+*/
 
 var writers = [
   {
@@ -36,10 +36,37 @@ var writers = [
     occupation: "writer",
     age: 64,
     alive: true
-  },
+  }
 ];
 
 /*
 If you want an extra challenge, only `console.log()` the writers that are alive.
 */
-if ()
+for (var i = 0; i < writers.length; i++) {
+  console.log(
+    "Hi, my name is " +
+      writers[i].firstName +
+      " " +
+      writers[i].lastName +
+      ". I am " +
+      writers[i].age +
+      " years old, and work as a " +
+      writers[i].occupation +
+      "."
+  );
+}
+for (var i = 0; i < writers.length; i++) {
+  if (writers[i].alive == true) {
+    console.log(
+      "Hi, my name is " +
+        writers[i].firstName +
+        " " +
+        writers[i].lastName +
+        ". I am " +
+        writers[i].age +
+        " years old, and work as a " +
+        writers[i].occupation +
+        "."
+    );
+  }
+}
